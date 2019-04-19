f052b34b561ebe484b301ac2a514995551fa90ba
Dan <dan.haymes@gmail.com>
2019-04-18 19:45:01 -0500

    added vue authentication

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Signup from "./views/Signup.vue"
+import Login from "./views/Login.vue"
+import Logout from "./views/Logout.vue"
+
 
 Vue.use(Router)
 
@@ -12,6 +16,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: "/Signup",
+      name: "signup",
+      component: Signup
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: Logout
     },
     {
       path: '/about',
